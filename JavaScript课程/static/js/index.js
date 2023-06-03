a1c646f72b2fc9906e32cf0a79cfb5c026f56364
William <bzx517291467@gmail.com>
2023-06-03 20:55:41 +0800

    JavaScript

---
+++ JavaScript课程/static/js/index.js
@@ -0,0 +1,35 @@
+let main = function () {
+    // let $div = $('div');
+    // let $btn_hide = $('#hide-btn');
+    // let $btn_show = $('#show-btn');
+    // $btn_hide.click(function () {
+    //     $div.hide();
+    // })
+    // $btn_show.click(function () {
+    //     $div.show();
+    // })
+    let $a = $('<a href="https://www.baidu.com">')
+    // let div = document.querySelector('div');
+    // console.log(div);
+
+    // let $div = $('.mydiv');
+    // console.log($div);
+    // let $div = $('div');
+    // $div.on("click.name1", function (e) {
+    //     console.log("click div");
+
+    //     // $div.off("click.name2");
+    // })
+    // $('a').on("click", function (e) {
+    //     // e.stopPropagation();
+    //     e.preventDefault();
+    // })
+    // $div.on("click.name2", function () {
+    //     console.log("click div2");
+    //     // $div.off("click");
+    // })
+}
+
+export {
+    main
+}
